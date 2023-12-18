afcb1e613341cd5b8c1aa1164bef8f6acbe97e61
FoxTail220 <134448019+FoxTail220@users.noreply.github.com>
2023-12-18 15:18:30 +0300

    Add files via upload

---
+++ bash-awscli.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+AWS_ACCESS_KEY_ID="AKIATSQNSBNZKJPBDP6F"
+AWS_SECRET_ACCESS_KEY="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"
+DEFAULT_REGION="eu-east-1"
+OUTPUT_FORMAT="json"
+
+
+OS=$(lsb_release -a | awk '/Distributor ID:/ {print $3}')
+
+
+if [ "$OS" = "RedHatEnterpriseServer" ]; then
+    echo "Red Hat Enterprise Server, installing aws cli using yum."
+    sudo yum install awscli -y
+    aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+    aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+    aws configure set default.region $DEFAULT_REGION
+    aws configure set default.output $OUTPUT_FORMAT
+    echo "+++++++++++++++ Запущенные инстансы +++++++++++++++"
+    aws ec2 describe-instances
+
+elif [ "$OS" = "Ubuntu" ]; then
+    echo "Ubuntu, installing aws cli using apt"
+    sudo apt-get update
+    sudo apt-get install awscli -y
+    aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+    aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+    aws configure set default.region $DEFAULT_REGION
+    aws configure set default.output $OUTPUT_FORMAT
+    echo "+++++++++++++++ Запущенные инстансы +++++++++++++++"
+    aws ec2 describe-instances
+
+elif [ "$OS" = "Darwin" ]; then
+    echo "MacOS, installing aws cli using Homebrew."
+    brew install awscli
+    aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+    aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+    aws configure set default.region $DEFAULT_REGION
+    aws configure set default.output $OUTPUT_FORMAT
+    echo "+++++++++++++++ Запущенные инстансы +++++++++++++++"
+    aws ec2 describe-instances
+
+elif [ "$OS" = "Microsoft" ]; then
+    echo "You need to turn on WSL module and install ubuntu subsystem"
+    echo "Windows, Please install aws cli using: bash current_script.sh"
+
+else
+    echo "Unsupported distribution: $OS"
+fi
